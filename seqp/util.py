592a0cad3802e99a88dd3d055f56a7f4bdb3895b
Noe Casas <noe.casas@gmail.com>
2019-03-25 15:45:34 +0000

    Add wrapper to add EOS

--- seqp/util.py
+++ seqp/util.py
@@ -4,6 +4,10 @@
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree.
 
+import numpy as np
+from seqp.record import RecordReader
+from typing import Iterable, Dict, Optional, Tuple, Union
+
 
 def count_lines(file_name: str) -> int:
     """
@@ -15,3 +19,47 @@ def count_lines(file_name: str) -> int:
         for i, l in enumerate(f):
             pass
     return i + 1
+
+
+class EosAppender(RecordReader):
+    """
+    Wrapper object to add EOS to the end of the sequences
+    and adjusts lengths accordingly.
+    """
+
+    def __init__(self, wrapped, eos):
+        super().__init__(wrapped.fields, wrapped.sequence_field)
+        self.wrapped  = wrapped
+        self.eos = eos
+
+    def close(self):
+        self.wrapped.close()
+
+    def indexes(self) -> Iterable[int]:
+        yield from self.wrapped.indexes()
+
+    def length(self, index) -> int:
+        return 1 + self.wrapped.length(index)
+
+    def indexes_and_lengths(self) -> Iterable[Tuple[int, int]]:
+        for i, l in self.wrapped.indexes_and_lengths():
+            yield i, l + 1
+
+    def retrieve(self, index) -> Union[np.ndarray, Dict[str, np.ndarray]]:
+        record = self.wrapped.retrieve(index)
+        sequence = (record if not self.wrapped.fields
+                    else record[self.wrapped.sequence_field])
+        sequence = np.append(sequence, self.eos)
+        if self.wrapped.fields:
+            record[self.wrapped.sequence_field] = sequence
+        else:
+            record = sequence
+
+        return record
+
+    def num_records(self) -> int:
+        return self.wrapped.num_records()
+
+    def metadata(self, metadata_key: str) -> Optional[str]:
+        return self.metadata(metadata_key)
+
