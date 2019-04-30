b5d20c654721587c90c5907c2c31843345a7920f
Noe Casas <noe.casas@ulgroup.com>
2019-04-30 11:13:48 +0100

    Avoid import cycle

--- seqp/integration/fairseq/__init__.py
+++ seqp/integration/fairseq/__init__.py
@@ -4,9 +4,8 @@
 # This source code is licensed under the license found in the LICENSE file in
 # the root directory of this source tree.
 
-from .task import SeqpTranslationTask
-from .dictionary import vocab_to_dictionary
-from .dataset import MonolingualDataset
+from seqp.integration.fairseq.dictionary import vocab_to_dictionary
+from seqp.integration.fairseq.dataset import MonolingualDataset
 
 
 
