83d2e51f76d0114c004f2878a976c612ae988159
Noe Casas <noe.casas@ulgroup.com>
2019-05-02 11:22:06 +0200

    Implement --nwordssrc and --nwordstgt options

--- seqp/integration/fairseq/preprocess.py
+++ seqp/integration/fairseq/preprocess.py
@@ -16,25 +16,55 @@ from seqp.hdf5 import Hdf5RecordWriter
 from seqp.util import count_lines
 
 
-def _create_vocab(sentences: Iterable[str]) -> Vocabulary:
+def _create_vocab(sentences: Iterable[str],
+                  num_tokens: int) -> Vocabulary:
+    """
+    Creates a Seqp vocabulary from the given sentences (which are assumed
+    to be tokenized, i.e. blank-separated tokens) and maximum number
+    of tokens.
+    :param sentences: Sentences (already tokenized) to create the vocabulary.
+    :param num_tokens: Maximum number of tokens in the vocabulary.
+    :return: A newly created vocabulary.
+    """
     builder = VocabularyCollector()
     for sentence in sentences:
         for symbol in sentence.strip().split(" "):
             builder.add_symbol(symbol)
-    vocab = builder.consolidate()
+
+    if num_tokens <= 0:
+        num_tokens = None
+    vocab = builder.consolidate(max_num_symbols=num_tokens)
     return vocab
 
 
-def _write_vocabs(train_prefix, src, tgt, destdir, joined_vocabs):
+def _write_vocabs(train_prefix: str,
+                  src: str,
+                  tgt: str,
+                  destdir: str,
+                  joined_vocabs: bool,
+                  num_src_tokens: int,
+                  num_tgt_tokens: int):
+    """
+    Writes source and target vocabularies (or a joint vocabulary)
+    :param train_prefix: Prefix of the training files (except lang extension)
+    :param src: Source language extension
+    :param tgt: Target language extension
+    :param destdir: Destination directory
+    :param joined_vocabs: Whether to use a joint source-target vocabulary.
+    :param num_src_tokens: Maximum number of tokens in the source vocabulary.
+    :param num_tgt_tokens: Maximum number of tokens in the target vocabulary.
+    :return: None
+    """
     import itertools
 
     src_file = f"{train_prefix}.{src}"
     tgt_file = f"{train_prefix}.{tgt}"
 
     if joined_vocabs:
+        num_tokens = num_src_tokens if num_src_tokens > 0 else num_tgt_tokens
         with open(src_file) as f1, open(tgt_file) as f2:
             sentences = itertools.chain(f1, f2)
-            vocab = _create_vocab(sentences)
+            vocab = _create_vocab(sentences, num_tokens)
 
         vocab_file = os.path.join(destdir, f'vocab.{src}-{tgt}')
         with open(vocab_file, 'w') as f:
@@ -43,12 +73,12 @@ def _write_vocabs(train_prefix, src, tgt, destdir, joined_vocabs):
         src_vocab = tgt_vocab = vocab
     else:
         with open(src_file) as f:
-            src_vocab = _create_vocab(f)
+            src_vocab = _create_vocab(f, num_src_tokens)
         src_vocab_file = os.path.join(destdir, f'vocab.{tgt}')
         with open(src_vocab_file, 'w') as f:
             f.write(src_vocab.to_json(indent=4))
         with open(tgt_file) as f:
-            tgt_vocab = _create_vocab(f)
+            tgt_vocab = _create_vocab(f, num_tgt_tokens)
         tgt_vocab_file = os.path.join(destdir, f'vocab.{src}')
         with open(tgt_vocab_file, 'w') as f:
             f.write(tgt_vocab.to_json(indent=4))
@@ -57,14 +87,23 @@ def _write_vocabs(train_prefix, src, tgt, destdir, joined_vocabs):
 
 
 def _writer(file_prefix: str,
-            suffix: str,
+            lang: str,
             num_sentences: int,
             max_records_per_shard: int) -> RecordWriter:
+    """
+    Creates an appropriate RecordWriter object.
+    :param file_prefix: File prefix where to store the data.
+    :param lang: Language extension.
+    :param num_sentences: Number of sentences to write to file (used
+                          to compute shards).
+    :param max_records_per_shard: Max amount of records per shard.
+    :return: RecordWriter object.
+    """
     if num_sentences < max_records_per_shard:
-        return Hdf5RecordWriter(f"{file_prefix}.{suffix}.hdf5")
+        return Hdf5RecordWriter(f"{file_prefix}.{lang}.hdf5")
     else:
         return ShardedWriter(Hdf5RecordWriter,
-                             file_prefix + ".{}." + suffix + ".hdf5",
+                             file_prefix + ".{}." + lang + ".hdf5",
                              max_records_per_shard)
 
 
@@ -84,6 +123,16 @@ def main():
     parser.add_argument("--trainpref", required=True)
     parser.add_argument("--validpref", required=True)
     parser.add_argument("--testpref", required=True)
+    parser.add_argument("--nwordssrc",
+                        metavar="N",
+                        default=-1,
+                        type=int,
+                        help="number of source words to retain")
+    parser.add_argument("--nwordstgt",
+                        metavar="N",
+                        default=-1,
+                        type=int,
+                        help="number of target words to retain")
     parser.add_argument("--destdir", required=True)
     parser.add_argument("--max-shard", type=int, default=400000)
     args = parser.parse_args()
@@ -98,7 +147,10 @@ def main():
                                          args.source_lang,
                                          args.target_lang,
                                          args.destdir,
-                                         args.joined_dictionary)
+                                         args.joined_dictionary,
+                                         args.nwordssrc,
+                                         args.nwordstgt,
+                                         )
 
     vocabs = {args.source_lang: src_vocab, args.target_lang: tgt_vocab}
 
